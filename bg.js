be92b6a10bcb66d2598a9fa6487f174ccdaf7290
dltndn <james98099@gmail.com>
2021-02-07 15:51:49 +0900

    test2

---
+++ bg.js
@@ -0,0 +1,14 @@
+const body = document.querySelector("body");
+
+function paintImage() {
+  const image = new Image();
+  image.src = "image/testBG.jpg";
+  image.classList.add("bgImage");
+  body.appendChild(image);
+}
+
+function init() {
+  paintImage();
+}
+
+init();
